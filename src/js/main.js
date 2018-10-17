1f15a607070b540f35563a9bea0b84b1fddd4d97
Pawel <pablomiklas@gmail.com>
2018-10-17 21:48:47 +0200

    Add responsive to cards and hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-import test from './ui/test';
-
-test();
+document.addEventListener("DOMContentLoaded", ()=>{
+    let hamburgerIcon = document.querySelector(".hamburger-display");
+    let hamburgerMenu = document.querySelector(".small-menu");
+    hamburgerIcon.addEventListener("click", ()=>{
+        hamburgerMenu.classList.toggle("hamburger-menu");
+        hamburgerMenu.classList.toggle("small-menu");
+    })
+})
