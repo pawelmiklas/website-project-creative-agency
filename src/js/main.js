234c3b0cb40316aa7c89b0d41adbc3549c0bc082
Pawel <pablomiklas@gmail.com>
2018-10-21 21:09:36 +0200

    Add responsive for whole website and improve menu in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    let hamburgerIcon = document.querySelector(".hamburger-display");
-    let hamburgerMenu = document.querySelector(".small-menu");
-    hamburgerIcon.addEventListener("click", ()=>{
-        hamburgerMenu.classList.toggle("hamburger-menu");
-        hamburgerMenu.classList.toggle("small-menu");
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  let hamburgerIcon = document.querySelector(".hamburger-display");
+  let hamburgerMenu = document.querySelector(".small-menu");
+  let cross = document.querySelector("#cross");
+  let main = document.querySelector("#main");
+  let hamburgerLink = document.querySelectorAll(".hamburger-link");
+  hamburgerIcon.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("hamburger-menu");
+    hamburgerIcon.classList.toggle("hamburger-display-none");
+    hamburgerMenu.classList.toggle("small-menu");
+    main.style.display = "none";
+  });
+  cross.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("hamburger-menu");
+    hamburgerIcon.classList.toggle("hamburger-display-none");
+    hamburgerMenu.classList.toggle("small-menu");
+    main.style.display = "block";
+  });
+  for (let i = 0; i < hamburgerLink.length; i++) {
+    hamburgerLink[i].addEventListener("click", () => {
+      main.style.display = "block";
+      hamburgerMenu.classList.toggle("hamburger-menu");
+      hamburgerIcon.classList.toggle("hamburger-display-none");
+      hamburgerMenu.classList.toggle("small-menu");
+    });
+  }
+});
